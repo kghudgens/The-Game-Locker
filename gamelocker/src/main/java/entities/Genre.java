52a9a53c9186a7915f7b69a45f6b20df1e595037
Kevin Hudgens <hudgens1073@gmail.com>
2022-10-23 22:19:13 -0500

    created enum class for the genres

---
+++ gamelocker/src/main/java/entities/Genre.java
@@ -0,0 +1,16 @@
+package entities;
+
+/**
+ * Enum class ties a new game entity to its respective genre
+ */
+public enum Genre {
+    SANDBOX,
+    SHOOTERS,
+    MULTIPLAYER,
+    ROLE_PLAYING_GAME,
+    SPORTS,
+    PUZZLE,
+    ACTION,
+    SURVIVAL_HORROR,
+    PLATFORMER
+}
