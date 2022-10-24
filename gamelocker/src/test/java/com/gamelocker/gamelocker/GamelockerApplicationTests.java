e29b1f81a5faccc1082b1b5b6ce21453314a4eba
Kevin Hudgens <hudgens1073@gmail.com>
2022-10-23 21:51:46 -0500

    Created spring application

---
+++ gamelocker/src/test/java/com/gamelocker/gamelocker/GamelockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamelocker.gamelocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamelockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
